de26be24fca5e9d19b802d7e3e8b0c021c543b87
parthjethava <jethavaparthp@gmail.com>
2026-07-26 03:18:09 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBBACjKMNxr2jzevolXW3GaqBOBdD4CJH4",
+  authDomain: "the-kiran-fashion.firebaseapp.com",
+  projectId: "the-kiran-fashion",
+  storageBucket: "the-kiran-fashion.firebasestorage.app",
+  messagingSenderId: "88989985858",
+  appId: "1:88989985858:web:4a2eede7bde64090d2d1db",
+};
+
+export const COLLECTIONS = {
+  users: "users",
+  products: "products",
+  orders: "orders",
+  stock: "stock",
+};
+
+export const BOOTSTRAP_ADMIN_EMAIL = "admin@kiranfashion.com";
